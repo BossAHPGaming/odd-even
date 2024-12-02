c7332f1b268decfc55aa58d2ef39f5b2546cbf7e
BossAHPGaming <aaravhpatel210@gmail.com>
2024-12-02 18:12:49 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+number = int(input("Enter Number to check: "))
+print("number to be checked :", number)
+
+if number%2==0 :
+    print("This is even number")
+else:
+    print("This is an odd number")
